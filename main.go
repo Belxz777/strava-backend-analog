44bebe37acf7d1aa1f12ca1fdc828987952ba9b3
Belxz777 <belxz888@gmail.com>
2025-06-06 20:16:29 +0400

    todo:subdiv to modules

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/Belxz777/backgo/common/db"
 	"github.com/Belxz777/backgo/logic/admin"
 	"github.com/Belxz777/backgo/logic/auth/register"
@@ -23,6 +26,7 @@ func main() {
 	h := db.Init(host, port, dbUser, dbPass, dbName)
 	register.RegisterRoutes(r, h)
 	admin.RegisterRoutes(r, h)
+	setupLogger(viper.Get("APP_ENV").(string))
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"port":  port,
@@ -31,4 +35,24 @@ func main() {
 	})
 
 	r.Run(":8888")
+}
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "dev":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "production":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
